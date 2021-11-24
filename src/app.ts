f34795cc7e00acacc01da00f576afbb94e02c1ad
Luiz Mendes <luiz.mendes@in8.com.br>
2021-11-23 21:36:01 -0300

    chore: add support to json requests

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocs from './api-docs/swagger.json';
+import userRoute from './infrastructure/controller/user';
 
 const app = express();
 
-app.use('/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use(express.json());
+app.use(cors());
 
+app.use('/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/v1/user', userRoute);
 app.use(express.json());
 
 export default app;
