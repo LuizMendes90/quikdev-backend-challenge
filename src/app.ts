46794d187e8ad4ea060c202fc96bf3f0fdf35bc5
Luiz Mendes <luiz.mendes@in8.com.br>
2021-11-23 21:00:16 -0300

    chore: add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './api-docs/swagger.json';
 
 const app = express();
+
+app.use('/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 
