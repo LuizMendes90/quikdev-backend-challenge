cca1b5df307829b1bd7f198f09d4eb9463e9ffa1
Luiz Mendes <luiz.mendes@in8.com.br>
2021-11-27 15:41:28 -0300

    test: mock to repository

---
+++ src/_test_/entity/MockTrueRepo.ts
@@ -0,0 +1,30 @@
+import User from '../../entity/User';
+import IUserRepository from '../../infrastructure/repository/IUserRepository';
+
+class MockTrueRepo implements IUserRepository {
+  async createUser(user: User): Promise<boolean> {
+    return true;
+  }
+
+  async getUserByUserName(user: User): Promise<boolean | User> {
+    return true;
+  }
+
+  async getUserById(user: User): Promise<boolean | User> {
+    return true;
+  }
+
+  async verifyById(id: string): Promise<boolean> {
+    return true;
+  }
+
+  async updateUser(id: string, user: User): Promise<boolean> {
+    return true;
+  }
+
+  async deleteUser(id: string): Promise<boolean> {
+    return true;
+  }
+}
+
+export default MockTrueRepo;
