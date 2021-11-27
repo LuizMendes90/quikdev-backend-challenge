0dc22f2238fc9cd310c7b1520ee09ce607d947c7
Luiz Mendes <luiz.mendes@in8.com.br>
2021-11-27 15:44:08 -0300

    refactor: move mock to sigle file

--- src/_test_/entity/user.spec.ts
+++ src/_test_/entity/user.spec.ts
@@ -1,35 +1,9 @@
 import User from '../../entity/User';
-import IUserRepository from '../../infrastructure/repository/IUserRepository';
-
-class MockRepo implements IUserRepository {
-  async createUser(user: User): Promise<boolean> {
-    return true;
-  }
-
-  async getUserByUserName(user: User): Promise<boolean | User> {
-    return true;
-  }
-
-  async getUserById(user: User): Promise<boolean | User> {
-    return true;
-  }
-
-  async verifyById(id: string): Promise<boolean> {
-    return true;
-  }
-
-  async updateUser(id: string, user: User): Promise<boolean> {
-    return true;
-  }
-
-  async deleteUser(id: string): Promise<boolean> {
-    return true;
-  }
-}
+import MockTrueRepo from './MockTrueRepo';
 
 describe('User', () => {
   it('should return true if user already exists', async () => {
-    const mock = new MockRepo();
+    const mock = new MockTrueRepo();
     const user = new User(mock);
     user.id = 'ad163ac6-4fa8-11ec-81d3-0242ac130003';
 
@@ -38,7 +12,7 @@ describe('User', () => {
   });
 
   it('should return false to create if user already exists', async () => {
-    const mock = new MockRepo();
+    const mock = new MockTrueRepo();
     const user = new User(mock);
     user.id = 'ad163ac6-4fa8-11ec-81d3-0242ac130003';
 
