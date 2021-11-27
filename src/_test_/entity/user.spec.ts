8a335b0149caa67968b99971cc7483e67e9e2c98
Luiz Mendes <luiz.mendes@in8.com.br>
2021-11-27 15:46:16 -0300

    test: ensure true if delete and user exist

--- src/_test_/entity/user.spec.ts
+++ src/_test_/entity/user.spec.ts
@@ -29,4 +29,13 @@ describe('User', () => {
     expect(exists).toBe(true);
   });
 
+  it('should return true to delete if user already exists', async () => {
+    const mock = new MockTrueRepo();
+    const user = new User(mock);
+    user.id = 'ad163ac6-4fa8-11ec-81d3-0242ac130003';
+
+    const exists = await user.delete(user.id);
+    expect(exists).toBe(true);
+  });
+
 });
