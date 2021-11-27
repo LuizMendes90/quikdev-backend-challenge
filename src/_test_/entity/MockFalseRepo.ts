0f62add375a29ab66e4467ff4ed62b4bd8fd964e
Luiz Mendes <luiz.mendes@in8.com.br>
2021-11-27 15:44:34 -0300

    test: add mock to repo

---
+++ src/_test_/entity/MockFalseRepo.ts
@@ -0,0 +1,30 @@
+import User from '../../entity/User';
+import IUserRepository from '../../infrastructure/repository/IUserRepository';
+
+class MockFalseRepo implements IUserRepository {
+  async createUser(user: User): Promise<boolean> {
+    return false;
+  }
+
+  async getUserByUserName(user: User): Promise<boolean | User> {
+    return false;
+  }
+
+  async getUserById(user: User): Promise<boolean | User> {
+    return false;
+  }
+
+  async verifyById(id: string): Promise<boolean> {
+    return false;
+  }
+
+  async updateUser(id: string, user: User): Promise<boolean> {
+    return false;
+  }
+
+  async deleteUser(id: string): Promise<boolean> {
+    return false;
+  }
+}
+
+export default MockFalseRepo;
