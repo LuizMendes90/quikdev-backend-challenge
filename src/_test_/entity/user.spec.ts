6d36615b8a6171b053ca79731525b579da834527
Luiz Mendes <luiz.mendes@in8.com.br>
2021-11-27 15:48:00 -0300

    test: ensure true if id sent exists in DB

--- src/_test_/entity/user.spec.ts
+++ src/_test_/entity/user.spec.ts
@@ -38,4 +38,12 @@ describe('User', () => {
     expect(exists).toBe(true);
   });
 
+  it('should return true to verify if id user already exists', async () => {
+    const mock = new MockTrueRepo();
+    const user = new User(mock);
+    user.id = 'ad163ac6-4fa8-11ec-81d3-0242ac130003';
+
+    const exists = await user.verifyExistById(user.id);
+    expect(exists).toBe(true);
+  });
 });
