2e8bdee21a863ec1836aac2cb7c31a1b367b2697
Luiz Mendes <luiz.mendes@in8.com.br>
2021-11-28 14:43:44 -0300

    fix: add correct validate to id

--- src/infrastructure/controller/user.ts
+++ src/infrastructure/controller/user.ts
@@ -43,8 +43,8 @@ userRoute.post('/', async (req: Request, res: Response) => {
 userRoute.get('/:id', async (req: Request, res: Response) => {
   const { id } = req.params;
 
-  if (!id) {
-    return res.status(400).send({ msg: 'ID is mandatory' });
+  if (!validator.isMongoId(id)) {
+    return res.status(400).send({ msg: 'Id must be a Correct Id' });
   }
 
   const repository = new UserRepository();
@@ -69,8 +69,8 @@ userRoute.put('/:id', async (req: Request, res: Response) => {
     description: req.body.description,
   };
 
-  if (!id) {
-    return res.status(400).send({ msg: 'ID is mandatory' });
+  if (!validator.isMongoId(id)) {
+    return res.status(400).send({ msg: 'Id must be a Correct Id' });
   }
   if (!isValidPhone(dto.primaryPhone))
     return res.status(400).send({ msg: 'Incorrect phone format!' });
@@ -93,8 +93,8 @@ userRoute.put('/:id', async (req: Request, res: Response) => {
 userRoute.delete('/:id', async (req: Request, res: Response) => {
   const { id } = req.params;
 
-  if (!id) {
-    return res.status(400).send({ msg: 'ID is mandatory' });
+  if (!validator.isMongoId(id)) {
+    return res.status(400).send({ msg: 'Id must be a Correct Id' });
   }
 
   const repository = new UserRepository();
